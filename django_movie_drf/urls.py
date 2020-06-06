388ddcfecc7cc99bcb4763d668f3bdd69f1ba1d3
Sergey Shekera <sergeyshekera98@gmail.com>
2020-06-06 22:34:30 +0300

    Added auto documentation by drf-yasg; swagger, redoc

--- django_movie_drf/urls.py
+++ django_movie_drf/urls.py
@@ -18,6 +18,9 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from .yasg import urlpatterns as doc_urls
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
@@ -27,6 +30,8 @@ urlpatterns = [
     path('auth/', include('djoser.urls.jwt')),
     path("api/v1/", include("movies.urls")),
 ]
+
+urlpatterns += doc_urls
 
 if settings.DEBUG:
     urlpatterns += static(
